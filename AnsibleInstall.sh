7a1d7887dc54017423024f1f93e36a83ce40cae4
Abhishek Mantripragada <42812595+abhimantri@users.noreply.github.com>
2021-12-08 16:28:54 +0000

    Create AnsibleInstall.sh

---
+++ AnsibleInstall.sh
@@ -0,0 +1,6 @@
+## Running on Ubuntu-Linux
+sudo apt-get update ##To update the system
+sudo apt-get install software-properties common ## To install common software properties
+sudo apt-add-repository ppa:/ansible/ansible
+sudo apt-get update
+sudo apt-get install ansible
